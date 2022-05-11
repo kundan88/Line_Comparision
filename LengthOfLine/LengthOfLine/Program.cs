edef631f448f8c7bd7c92cd96511866dd0712566
Kundan <kundan kamble>
2022-05-11 04:43:46 -0700

    [Kundan] Added Lenth of line program

---
+++ LengthOfLine/LengthOfLine/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//UC1
+using System;
+
+namespace Line_Comparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome to Line Comparision Program");
+            Console.WriteLine();
+            Console.WriteLine("Enter the coordinates: ");
+
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+
+            double line1 = Math.Sqrt((Math.Pow((x2 - x1), 2)) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(" length: " + Math.Round(line1, 3));
+
+
+        }
+    }
+}
+
